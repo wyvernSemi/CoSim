f13a5a2fc91fe8d23a8c33e63d2b4d8c83235466
Jim Lewis <jim@synthworks.com>
2023-01-25 07:02:46 -0800

    Updated copyright notices

--- include/rv32_cpu_gdb.h
+++ include/rv32_cpu_gdb.h
@@ -1,25 +1,38 @@
-//=============================================================
-// 
-// Copyright (c) 2021 Simon Southwell. All rights reserved.
+// =========================================================================
 //
-// Date: 5th August 2021
+//  File Name:         rv32_cpu_gdb.h
+//  Design Unit Name:
+//  Revision:          OSVVM MODELS STANDARD VERSION
 //
-// This file is part of the rv32_cpu instruction set simulator.
+//  Maintainer:        Simon Southwell email:  simon.southwell@gmail.com
+//  Contributor(s):
+//    Simon Southwell      simon.southwell@gmail.com
 //
-// This code is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+//  Description:
+//    This file is part of the rv32_cpu instruction set simulator.
 //
-// The code is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+//  Revision History:
+//    Date      Version    Description
+//    01/2023   2023.01    Released with OSVVM CoSim
+//    5th August 2021      Earlier version
 //
-// You should have received a copy of the GNU General Public License
-// along with this code. If not, see <http://www.gnu.org/licenses/>.
+//  This file is part of OSVVM.
 //
-//=============================================================
+//  Copyright (c) 2021 Simon Southwell. 
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+// =========================================================================
 
 #ifndef _RV32_CPU_GDB_H_
 #define _RV32_CPU_GDB_H_
