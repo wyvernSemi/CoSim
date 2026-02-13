f94cd85826598cbef8b4b71675c6f8fa43c083c3
Simon Southwell <simon@anita-simulators.org.uk>
2026-02-13 14:20:58 +0000

    Added some warning suppress command options of compiling of Altera PCIe IP.

--- Scripts/MakeVproc.tcl
+++ Scripts/MakeVproc.tcl
@@ -43,11 +43,12 @@ namespace eval ::osvvm {
 
 proc AlteraLibArgs {} {
 
-  return "-L work          -L work_lib          -L pcie_cv_hip_avmm_0    -L altera_ver        \
-          -L lpm_ver       -L sgate_ver         -L altera_mf_ver         -L altera_lnsim_ver  \
-          -L cyclonev_ver  -L cyclonev_hssi_ver -L cyclonev_pcie_hip_ver -L altera            \
-          -L lpm           -L sgate             -L altera_mf             -L altera_lnsim      \
-          -L cyclonev      -L cyclonev_hssi"
+  return "-L work          -L pcie_cv_hip_avmm_0 -L altera_ver                                  \
+          -L lpm_ver       -L sgate_ver          -L altera_mf_ver         -L altera_lnsim_ver   \
+          -L cyclonev_ver  -L cyclonev_hssi_ver  -L cyclonev_pcie_hip_ver -L altera             \
+          -L lpm           -L sgate              -L altera_mf             -L altera_lnsim       \
+          -L cyclonev      -L cyclonev_hssi                                                     \
+          -suppress 2241   -suppress 2685         -suppress 2718          -suppress 2958"
 }
 
 # -------------------------------------------------------------------------
